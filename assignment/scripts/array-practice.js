d38b5780f26fccae42678c3417c705d8a540cb6b
Linssi Colin Ruiz <linssi101@yahoo.com>
2022-06-19 20:02:02 -0500

    added code to question 1

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -8,6 +8,8 @@ let animalArray = ['fish', 'cat', 'bird', 'dog'];
 console.log('Animals are: ', animalArray);
 
 // 1.a. TODO: Make an array with some favorite foods
+let favFood = ['Hot Pot', 'Tacos', 'Pizza', 'Noodles']
+console.log("My favorite foods are: ", favFood)
 
 
 // 1.b. TODO: Log your array of foods to the console with a message, similar 
